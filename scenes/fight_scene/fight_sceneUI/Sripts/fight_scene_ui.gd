c2fe7053fdf469ccefda7f775ef0052284d3c8df
AnselTK <odinochoa1@gmail.com>
2026-01-14 21:25:00 -0800

    Revert "c"
    This reverts commit 5004b73298da964c7324d8f9ae4cfa5b32a42263.

--- scenes/fight_scene/fight_sceneUI/Sripts/fight_scene_ui.gd
+++ scenes/fight_scene/fight_sceneUI/Sripts/fight_scene_ui.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 	$"../../Bot1/bot".texture_normal =enemi.texture
 	
 	Life_Player =State.HP_Actual
-	Life_Bot =enemi.HP
+	Life_Bot =enemi.Hp
 	
 	$Notificaciones.hide()
 	$HBoxContainer/NinePatchRect.hide()
