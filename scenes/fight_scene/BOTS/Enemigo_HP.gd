c1c1c02c5f710d7d491b08cf17c311276e2b7449
AnselTK <odinochoa1@gmail.com>
2026-01-14 19:29:05 -0800

    c

--- scenes/fight_scene/BOTS/Enemigo_HP.gd
+++ scenes/fight_scene/BOTS/Enemigo_HP.gd
@@ -2,6 +2,6 @@ class_name Enemy extends Resource
 
 
 @export var name: String = "Enemy"
-@export var texture: Texture
+@export var texture: Texture2D
 @export var HP: int = 30
 @export var damage: int = 20
