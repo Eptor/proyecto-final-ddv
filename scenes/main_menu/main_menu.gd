ad833ae6bab3900764309c78d97c501b509483e7
AnselTK <odinochoa1@gmail.com>
2026-01-16 13:56:58 -0800

    c

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,9 +1,10 @@
 extends Control
-
+@onready var s =$Star
 var main_level_scene = String("res://scenes/main_world/main_world.tscn")
 var credits_scene = String("res://scenes/credits/credits.tscn")
 
 func _on_start_button_pressed() -> void:
+	s.play()
 	SceneChanger.fade(main_level_scene)
 
 func _on_exit_button_pressed() -> void:
